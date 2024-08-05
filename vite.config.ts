0edfe7e7a4981b24e6b27fe00732c6c5cf049692
Vallis <p.vallis@rasa.com>
2024-08-05 20:51:25 +0100

    Configure PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,30 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Supatask",
+        short_name: "Supatask",
+        description: "Get on top of your to do list!",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/supatask/",
 });
